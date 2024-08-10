11b8321624c71f7cd8544faefed57b6184bcc3d8
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-10 17:34:58 +0700

    Constant

---
+++ Constant.php
@@ -0,0 +1,12 @@
+<?php
+
+// Constant
+define("AUTHOR", "Fakhri Rossi");
+define("APP_VERSION", 1.0);
+
+echo "Author\t\t: ";
+echo AUTHOR;
+echo "\n";
+echo "App Version\t: ";
+echo APP_VERSION;
+echo "\n";
