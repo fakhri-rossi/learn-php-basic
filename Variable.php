89d541c95901ff1bd9ac82142370cc2eef87888d
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-10 17:32:10 +0700

    Variable

---
+++ Variable.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = "Eka";
+$age = 20;
+
+echo "Name: ";
+echo $name;
+echo "\n";
+
+echo "Age: ";
+echo $age;
+echo "\n";
+
+
+// variable variables
+$name = "rossi";
+$$name = "fakhri";
+
+echo "\$name = ";
+echo $name;
+echo "\n";
+
+echo "\$rossi = ";
+echo $rossi;
+echo "\n";
