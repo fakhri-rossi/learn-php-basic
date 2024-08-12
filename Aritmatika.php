e202d5ea97f850758f99c3a628797a09c5c7569b
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-12 18:59:51 +0700

    Aritmatika

---
+++ Aritmatika.php
@@ -0,0 +1,30 @@
+<?php
+
+$num1 = 5;
+$num2 = 3;
+
+// penumlahan
+$result = $num1 + $num2;
+var_dump($result);
+
+// pengurangan
+$result = $num1 - $num2;
+var_dump($result);
+
+// perkalian
+$result = $num1 * $num2;
+var_dump($result);
+
+// pembagian
+$result = $num1 / $num2;
+var_dump($result);
+
+// modulo
+$result = $num1 % $num2;
+var_dump($result);
+
+// pangkat
+$result = $num1 ** $num2;
+var_dump($result);
+
+?>
