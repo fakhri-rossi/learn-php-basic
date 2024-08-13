c5c10ffc0f85d5739081fe095cee50148a4e24b1
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-13 12:58:15 +0700

    Operator Logika

---
+++ OperatorLogika.php
@@ -0,0 +1,15 @@
+<?php
+
+var_dump(true or false);
+var_dump(true || false);
+
+var_dump(true and false);
+var_dump(true && false);
+
+var_dump(true xor false);
+var_dump(true || false);
+
+var_dump(!true);
+var_dump(!false);
+
+?>
